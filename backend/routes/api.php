dc1ae3ab857d178ca7f4d154d6604f23537693d5
Gustavo Ferreira <102172136+gustapng@users.noreply.github.com>
2025-08-10 18:23:46 -0300

    adding register feature and mailpit service

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Route;
 use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 Route::post('/register', function (Request $request) {
     $request->validate([
@@ -22,11 +23,14 @@ Route::post('/register', function (Request $request) {
         'password' => Hash::make($request->password),
     ]);
 
+    $user->sendEmailVerificationNotification();
+
     $token = $user->createToken('auth_token')->plainTextToken;
 
     return response()->json([
         'access_token' => $token,
         'token_type' => 'Bearer',
+        'message' => 'Usuário registrado com sucesso. Por favor, verifique seu e-mail para ativar sua conta.'
     ]);
 });
 
@@ -57,3 +61,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/sanctum/csrf-cookie', [CsrfCookieController::class, 'show']);
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return response()->json(['message' => 'E-mail verificado com sucesso!']);
+})->middleware(['auth:sanctum'])->name('verification.verify');
--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,11 +1,48 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import InputBase from '@/components/inputs/InputBase.vue'
+import InputPassword from '@/components/inputs/InputPassword.vue'
+import RoundedButton from './buttons/RoundedButton.vue'
+
+const email = ref('')
+const password = ref('')
+
+const errors = ref<Record<string, string[]>>({});
+</script>
+
 <template>
     <div class="header">
-        <header class="py-5">
-            <RouterLink 
-                to="/register" 
-                class="rounded-full bg-primaryColor hover:bg-black p-3 text-black hover:text-white border border-black">
-                Demonstração
-            </RouterLink>
+        <header class="py-5 flex justify-between items-center">
+            <nav>
+                <RouterLink 
+                    to="/register" 
+                    class="rounded-full bg-primaryColor hover:bg-black p-3 text-black hover:text-white border border-black inline-block">
+                    Demonstração
+                </RouterLink>
+            </nav>
+            <div class="flex items-center space-x-4">
+                <InputBase
+                    id="email"
+                    label="E-mail"
+                    type="email"
+                    placeholder="Digite seu e-mail"
+                    v-model="email"
+                    :required="true"
+                    :error-message="errors.email ? errors.email[0] : ''"
+                    class="w-full"
+                />
+                <InputPassword
+                    id="password"
+                    label="Senha"
+                    type="password"
+                    placeholder="Digite sua senha"
+                    v-model="password"
+                    :required="true"
+                    :error-message="errors.password ? errors.password[0] : ''"
+                    class="w-full"
+                />
+                <RoundedButton type="submit">Login</RoundedButton>
+            </div>
         </header>
     </div>
 </template>
